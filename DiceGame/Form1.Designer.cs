a32a5da6ef18ca3ec2b186f46c8f19cada081675
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2021-03-02 09:39:15 +1300

    Add project files.

---
+++ DiceGame/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace DiceGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pbDiceShow1 = new System.Windows.Forms.PictureBox();
+            this.pbDiceShow2 = new System.Windows.Forms.PictureBox();
+            this.pbDiceShow3 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceShow1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceShow2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceShow3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(416, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll Dice Game";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 145);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DiceGame.Resource1.two;
+            this.pictureBox2.Location = new System.Drawing.Point(172, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 145);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::DiceGame.Resource1.four;
+            this.pictureBox3.Location = new System.Drawing.Point(523, 55);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(156, 134);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::DiceGame.Resource1.three;
+            this.pictureBox4.Location = new System.Drawing.Point(342, 55);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(159, 145);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::DiceGame.Resource1.five;
+            this.pictureBox5.Location = new System.Drawing.Point(709, 55);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(157, 145);
+            this.pictureBox5.TabIndex = 1;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::DiceGame.Resource1.six;
+            this.pictureBox6.Location = new System.Drawing.Point(902, 55);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(187, 155);
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pbDiceShow1
+            // 
+            this.pbDiceShow1.Location = new System.Drawing.Point(38, 226);
+            this.pbDiceShow1.Name = "pbDiceShow1";
+            this.pbDiceShow1.Size = new System.Drawing.Size(190, 164);
+            this.pbDiceShow1.TabIndex = 2;
+            this.pbDiceShow1.TabStop = false;
+            // 
+            // pbDiceShow2
+            // 
+            this.pbDiceShow2.Location = new System.Drawing.Point(294, 226);
+            this.pbDiceShow2.Name = "pbDiceShow2";
+            this.pbDiceShow2.Size = new System.Drawing.Size(174, 160);
+            this.pbDiceShow2.TabIndex = 3;
+            this.pbDiceShow2.TabStop = false;
+            // 
+            // pbDiceShow3
+            // 
+            this.pbDiceShow3.Location = new System.Drawing.Point(523, 226);
+            this.pbDiceShow3.Name = "pbDiceShow3";
+            this.pbDiceShow3.Size = new System.Drawing.Size(181, 170);
+            this.pbDiceShow3.TabIndex = 4;
+            this.pbDiceShow3.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnStart.Location = new System.Drawing.Point(749, 216);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(130, 50);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.DarkRed;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExit.Location = new System.Drawing.Point(749, 286);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(138, 58);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(558, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 574);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbDiceShow3);
+            this.Controls.Add(this.pbDiceShow2);
+            this.Controls.Add(this.pbDiceShow1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceShow1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceShow2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceShow3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pbDiceShow1;
+        private System.Windows.Forms.PictureBox pbDiceShow2;
+        private System.Windows.Forms.PictureBox pbDiceShow3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DiceGame/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DiceGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sndPlayer = new SoundPlayer(Resource1.DICEUP);
+            sndPlayer.Play();
+
+            Random random1 = new Random(); //this is the library method to generate a random number
+            int randno = random1.Next(1, 7);//the variable randno will get a random value between 0, 1, 2, 3, 4, 5
+            label2.Text = Convert.ToString(randno);
+            if (randno == 1)
+            {
+                pbDiceShow1.Image = Resource1.one;
+            }
+            if(randno == 2)
+            {
+                pbDiceShow1.Image = Resource1.two;
+            }
+            if (randno == 3)
+            {
+                pbDiceShow1.Image = Resource1.three;
+            }
+            if (randno == 4)
+            {
+                pbDiceShow1.Image = Resource1.four;
+            }
+            if (randno == 5)
+            {
+                pbDiceShow1.Image = Resource1.five;
+            }
+            if (randno == 6)
+            {
+                pbDiceShow1.Image = Resource1.six;
+            }
+            randno = random1.Next(1, 7);//the variable randno will get a random value between 0, 1, 2, 3, 4, 5
+            label3.Text = Convert.ToString(randno);
+            if (randno == 1)
+            {
+                pbDiceShow2.Image = Resource1.one;
+            }
+            if (randno == 2)
+            {
+                pbDiceShow2.Image = Resource1.two;
+            }
+            if (randno == 3)
+            {
+                pbDiceShow2.Image = Resource1.three;
+            }
+            if (randno == 4)
+            {
+                pbDiceShow2.Image = Resource1.four;
+            }
+            if (randno == 5)
+            {
+                pbDiceShow2.Image = Resource1.five;
+            }
+            if (randno == 6)
+            {
+                pbDiceShow2.Image = Resource1.six;
+            }
+            randno = random1.Next(1, 7);//the variable randno will get a random value between 0, 1, 2, 3, 4, 5
+            label4.Text = Convert.ToString(randno);
+            if (randno == 1)
+            {
+                pbDiceShow3.Image = Resource1.one;
+            }
+            if (randno == 2)
+            {
+                pbDiceShow3.Image = Resource1.two;
+            }
+            if (randno == 3)
+            {
+                pbDiceShow3.Image = Resource1.three;
+            }
+            if (randno == 4)
+            {
+                pbDiceShow3.Image = Resource1.four;
+            }
+            if (randno == 5)
+            {
+                pbDiceShow3.Image = Resource1.five;
+            }
+            if (randno == 6)
+            {
+                pbDiceShow3.Image = Resource1.six;
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
